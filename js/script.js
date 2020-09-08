5742dc7d60ebc2156f5d2e7d07bac892d7c7b1f5
unknown <Pushpa31>
2020-09-08 11:05:52 +0545

    home

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// $(function () {
+//   $(".dropdown-menu a").click(function () {
+//     console.log($(this).attr("data-value"));
+//     $(this)
+//       .closest(".dropdown")
+//       .find("input.countrycode")
+//       .val("(" + $(this).attr("data-value") + ")");
+//   });
+// });
