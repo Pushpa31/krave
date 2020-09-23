2895795f26c701bb9b130890040ddff86c64b283
unknown <Pushpa31>
2020-09-23 18:15:17 +0545

    updated

--- js/script.js
+++ js/script.js
@@ -12,15 +12,16 @@ function scrollFunction() {
     document.getElementById("resturantid").style.height = "13.5em";
     document.getElementById("resturantid").style.width = "100vw";
     document.getElementById("resturantid").style.marginLeft = "-190px";
-    document.getElementById("resturantid").style.backgroundColor =
-      "rgb(200,230,221)";
+    document.getElementById("resturantid").style.backgroundColor = "#e8e8e8";
     document.getElementById("rest-detail").style.paddingTop = "0em";
     document.getElementById("rest-fav").style.paddingTop = "0em";
     document.getElementById("rest-address").style.fontSize = "1em";
-    document.getElementById("rest-button").style.width = "6em";
-    document.getElementById("rest-button").style.height = "2em";
+    document.getElementById("rest-button").style.width = "90px";
+    document.getElementById("rest-button").style.height = "35px";
+    document.getElementById("rest-button").style.fontSize = "0.9rem";
     document.getElementById("rest-button").style.backgroundColor = "#5ebd16";
     document.getElementById("ul-down").style.fontSize = "0.8em";
+    document.getElementById("time-id").style.color = "white";
     document.querySelector(".rest-searchbox").style.border = "1px solid gray";
     document.querySelector(".rest-searchbox").style.borderRadius = "25px";
     document.querySelector(".rest-searchbox").style.outline = "none";
@@ -44,10 +45,11 @@ function scrollFunction() {
     document.getElementById("resturantid").style.width = "100%";
     document.getElementById("resturantid").style.marginLeft = "0";
     document.getElementById("rest-fav").style.paddingTop = "15px";
-    document.getElementById("resturantid").style.height = "33em";
+    document.getElementById("resturantid").style.height = "30em";
     document.getElementById("resturantid").style.backgroundColor = "white";
+    document.getElementById("f-title").style.color = "unset";
     document.getElementById("f-title").style.fontSize = "2em";
-    document.getElementById("ul-down").style.fontSize = "1.3em";
+    document.getElementById("ul-down").style.fontSize = "1rem";
     document.getElementById("rest-address").style.fontSize = "1.3em";
     document.getElementById("rest-detail").style.paddingTop = "20px";
     document.getElementById("rest-detail").style.paddingLeft = "46px";
@@ -57,6 +59,8 @@ function scrollFunction() {
     document.querySelector(".rest-searchbox").style.outline = "none";
     document.getElementById("rest-button").style.width = "8em";
     document.getElementById("rest-button").style.height = "2.7em";
+    document.getElementById("rest-button").style.fontSize = "unset";
+
     document.querySelector(".rest-stars").style.fontSize = "1.1rem";
 
     // document.getElementById("resturantid").style.backgroundColor = "white";
